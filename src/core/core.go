55668c89faba1fa9859b78e10f694036e382d05e
harvestcore <aagomezies@gmail.com>
2020-11-01 20:15:25 +0100

    Create basic Core struct. Closes #21. Continues #16, #17, #31, #32

---
+++ src/core/core.go
@@ -0,0 +1,28 @@
+package core
+
+type Core struct {
+}
+
+func (c Core) CreateUpdater() {
+
+}
+
+func (c Core) UpdateData() {
+
+}
+
+func (c Core) StoreData() {
+
+}
+
+func (c Core) FetchData() {
+
+}
+
+func (c Core) HandleEvent() {
+
+}
+
+func (c Core) SendEvent() {
+
+}
