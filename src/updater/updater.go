72c8be738b00a37deb55a12ec1a7f36f5c7c9cbd
harvestcore <aagomezies@gmail.com>
2020-11-04 00:27:41 +0100

    Basic FetchData method working. Continues #37 (HU #15)

--- src/updater/updater.go
+++ src/updater/updater.go
@@ -1,14 +1,23 @@
 package updater
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type Updater struct {
-	Schema   map[interface{}]interface{}
-	Interval uint
-	Source   string
-	Id       uuid.UUID
+	Schema      map[interface{}]interface{}
+	Interval    int
+	Source      string
+	ID          uuid.UUID
+	Method      string
+	RequestBody map[string]interface{}
+	Timeout     int
 }
 
 func (u Updater) SendUpdate() {
@@ -19,6 +28,55 @@ func (u Updater) HandleEvent() {
 
 }
 
-func (u Updater) FetchData() {
+func (u Updater) GetClient() (client *http.Client) {
+	timeout := time.Duration(u.Timeout) * time.Second
+
+	client = &http.Client{
+		Timeout: timeout,
+	}
+
+	return
+}
+
+// FetchData Fetches the data from the source
+func (u Updater) FetchData() map[string]interface{} {
+	var requestBody []byte
+	var requestBodyErr error
+	var body bytes.Buffer
+	var dat map[string]interface{}
+
+	if u.Method == "POST" {
+		requestBody, requestBodyErr = json.Marshal(u.RequestBody)
+		body = *bytes.NewBuffer(requestBody)
+	}
+
+	if requestBodyErr == nil {
+		request, requestErr := http.NewRequest(u.Method, u.Source, &body)
+
+		if requestErr == nil {
+			request.Header.Set("Content-type", "application/json")
+
+			response, responseErr := u.GetClient().Do(request)
+
+			if responseErr == nil {
+				defer response.Body.Close()
+
+				responseBody, responseBodyErr := ioutil.ReadAll(response.Body)
+
+				if responseBodyErr == nil {
+					unmarshalErr := json.Unmarshal(responseBody, &dat)
+
+					if unmarshalErr == nil {
+						return dat
+					}
+				}
+			}
+		}
+	}
+
+	return dat
+}
+
+func (u Updater) Run() {
 
 }
