691f1874e67f6dbe6e2603ea3e7e608934dba827
harvestcore <aagomezies@gmail.com>
2020-11-07 23:30:04 +0100

    Add some comments. Closes #37

--- src/updater/updater.go
+++ src/updater/updater.go
@@ -28,6 +28,7 @@ func (u Updater) HandleEvent() {
 
 }
 
+// GetClient Returns the client configured with the timeout inverval
 func (u Updater) GetClient() (client *http.Client) {
 	timeout := time.Duration(u.Timeout) * time.Second
 
@@ -45,25 +46,31 @@ func (u Updater) FetchData() map[string]interface{} {
 	var body bytes.Buffer
 	var dat map[string]interface{}
 
+	// If the method is POST -> encode the request body
 	if u.Method == "POST" {
 		requestBody, requestBodyErr = json.Marshal(u.RequestBody)
 		body = *bytes.NewBuffer(requestBody)
 	}
 
 	if requestBodyErr == nil {
+		// Create request object
 		request, requestErr := http.NewRequest(u.Method, u.Source, &body)
 
 		if requestErr == nil {
+			// Set header content type and perform the request
 			request.Header.Set("Content-type", "application/json")
-
 			response, responseErr := u.GetClient().Do(request)
 
 			if responseErr == nil {
+				// Defer the close of the body. It will be closed as soon as
+				// this method ends
 				defer response.Body.Close()
 
+				// Read the body
 				responseBody, responseBodyErr := ioutil.ReadAll(response.Body)
 
 				if responseBodyErr == nil {
+					// Decode the body
 					unmarshalErr := json.Unmarshal(responseBody, &dat)
 
 					if unmarshalErr == nil {
