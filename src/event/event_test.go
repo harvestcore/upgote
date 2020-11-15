f2e1ad8846a0e3068a2b9a793b1bf53427c7f110
harvestcore <aagomezies@gmail.com>
2020-11-15 20:37:30 +0100

    Add event tests. Fixes #61

---
+++ src/event/event_test.go
@@ -0,0 +1,68 @@
+package event_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/harvestcore/HarvestCCode/src/event"
+	"github.com/harvestcore/HarvestCCode/src/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventCreation(t *testing.T) {
+	var niluuid uuid.UUID
+	var nildata map[interface{}]interface{}
+
+	var from = uuid.New()
+	var to = uuid.New()
+	var data = make(map[interface{}]interface{}, 0)
+
+	var e = event.NewEvent(
+		from,
+		to,
+		utils.Store,
+		"action",
+		data,
+	)
+
+	assert.NotNil(t, e, "Event creation return nil with all parameters set")
+	assert.NotNil(t, e.ID, "ID is nil")
+	assert.Equal(t, e.From, from, "Mismatch 'from' UUID")
+	assert.Equal(t, e.To, to, "Mismatch 'to' UUID")
+	assert.Equal(t, e.Type, utils.EventType("store"), "Mismatch event type")
+	assert.Equal(t, e.Action, "action", "Mismatch action")
+	assert.NotNil(t, e.Data, "Data map is nil")
+	assert.Equal(t, len(e.Data), 0, "Data map length is not 0")
+
+	e = event.NewEvent(
+		niluuid,
+		to,
+		utils.Store,
+		"action",
+		data,
+	)
+
+	assert.Nil(t, e, "Event created with nil 'from'")
+
+	e = event.NewEvent(
+		from,
+		niluuid,
+		utils.Store,
+		"action",
+		data,
+	)
+
+	assert.Nil(t, e, "Event created with nil 'to'")
+
+	e = event.NewEvent(
+		from,
+		to,
+		utils.Store,
+		"action",
+		nildata,
+	)
+
+	assert.NotNil(t, e, "Event not created with nil 'data'")
+	assert.NotNil(t, e.Data, "Data map is nil")
+	assert.Equal(t, len(e.Data), 0, "Data map length is not 0")
+}
