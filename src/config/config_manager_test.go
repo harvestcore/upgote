ea24a6fc336b69e8bdac10613ca73711c3af17b8
harvestcore <aagomezies@gmail.com>
2020-11-22 18:47:36 +0100

    Config manager Unit tests. Continues #66 and #16

---
+++ src/config/config_manager_test.go
@@ -0,0 +1,22 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/harvestcore/HarvestCCode/src/config"
+)
+
+func TestConfigManager(t *testing.T) {
+	var cm = config.GetConfigManager()
+	assert.NotNil(t, cm, "Config manager creation returned nil")
+
+	assert.NotEqual(t, cm.Database, "", "Database is empty")
+	assert.NotEqual(t, cm.MongoPort, "", "MongoPort is empty")
+	assert.NotEqual(t, cm.MongoHost, "", "MongoHost is empty")
+	assert.NotEqual(t, cm.MongoURI, "", "MongoURI is empty")
+
+	var cm2 = config.GetConfigManager()
+	assert.Equal(t, cm, cm2, "A 'second' instance is not the same")
+}
