30ce30908c7bcb995b010bde4c80f48904051866
harvestcore <aagomezies@gmail.com>
2020-12-08 21:04:11 +0100

    Integrate etcd3. Closes #73

--- src/config/config_manager.go
+++ src/config/config_manager.go
@@ -1,8 +1,21 @@
 package config
 
 import (
+	"context"
 	"os"
 	"sync"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+type Variable string
+
+const (
+	HCC_MONGO_PORT     Variable = "HCC_MONGO_PORT"
+	HCC_MONGO_HOST     Variable = "HCC_MONGO_HOST"
+	HCC_MONGO_DATABASE Variable = "HCC_MONGO_DATABASE"
+	HCC_MONGO_URI      Variable = "HCC_MONGO_URI"
 )
 
 var lock = &sync.Mutex{}
@@ -13,6 +26,9 @@ type Manager struct {
 	MongoHost string
 	MongoURI  string
 	Database  string
+	Context   context.Context
+	Client    *clientv3.Client
+	KV        clientv3.KV
 }
 
 var manager *Manager
@@ -22,6 +38,12 @@ func GetManager() *Manager {
 	if manager == nil {
 		lock.Lock()
 		defer lock.Unlock()
+
+		context, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		client, _ := clientv3.New(clientv3.Config{
+			DialTimeout: 2 * time.Second,
+			Endpoints:   []string{"127.0.0.1:2379"},
+		})
 
 		var port = os.Getenv("HCC_MONGO_PORT")
 		if port == "" {
@@ -48,8 +70,22 @@ func GetManager() *Manager {
 			MongoHost: host,
 			MongoURI:  mongoURI,
 			Database:  database,
+			Context:   context,
+			Client:    client,
+			KV:        clientv3.NewKV(client),
 		}
 	}
 
 	return manager
+}
+
+// GetVariable Returns the requested variable.
+func (manager *Manager) GetVariable(variable Variable) string {
+	var _var string
+	data, _ := manager.KV.Get(manager.Context, string(variable))
+	for _, ev := range data.Kvs {
+		_var = string(ev.Value)
+	}
+
+	return _var
 }
