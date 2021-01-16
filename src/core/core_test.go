fa99e8d76c383fa10533e2cfc5d878e9d9bf9ca1
harvestcore <aagomezies@gmail.com>
2021-01-16 23:31:23 +0100

    More docker checks

--- src/core/core_test.go
+++ src/core/core_test.go
@@ -4,8 +4,10 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/harvestcore/HarvestCCode/src/core"
 	"github.com/stretchr/testify/assert"
+	
+	"github.com/harvestcore/HarvestCCode/src/core"
+	"github.com/harvestcore/HarvestCCode/src/utils"
 )
 
 func TestCoreCreation(t *testing.T) {
@@ -17,71 +19,77 @@ func TestCoreCreation(t *testing.T) {
 }
 
 func TestCoreCreateUpdater(t *testing.T) {
-	var schema = make(map[string]interface{})
-	schema["cool"] = "test"
+	if !utils.RunningInDocker() {
+		var schema = make(map[string]interface{})
+		schema["cool"] = "test"
 
-	var data = make(map[string]interface{})
-	data["collection"] = "testing"
-	data["schema"] = schema
-	data["interval"] = 10
-	data["source"] = "https://google.es"
-	data["method"] = "GET"
-	data["requestBody"] = make(map[string]interface{})
-	data["timeout"] = 20
+		var data = make(map[string]interface{})
+		data["collection"] = "testing"
+		data["schema"] = schema
+		data["interval"] = 10
+		data["source"] = "https://google.es"
+		data["method"] = "GET"
+		data["requestBody"] = make(map[string]interface{})
+		data["timeout"] = 20
 
-	var c = core.GetCore()
+		var c = core.GetCore()
 
-	var updater = c.CreateUpdater(data)
-	assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
-	assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
+		var updater = c.CreateUpdater(data)
+		assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
+		assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
 
-	c.Updaters = make(map[uuid.UUID]*core.UpdaterMap)
+		c.Updaters = make(map[uuid.UUID]*core.UpdaterMap)
+	}
 }
 
 func TestCoreStopUpdater(t *testing.T) {
-	var schema = make(map[string]interface{})
-	schema["cool"] = "test"
+	if !utils.RunningInDocker() {
+		var schema = make(map[string]interface{})
+		schema["cool"] = "test"
 
-	var data = make(map[string]interface{})
-	data["collection"] = "yikes"
-	data["schema"] = schema
-	data["interval"] = 10
-	data["source"] = "https://google.es"
-	data["method"] = "GET"
-	data["requestBody"] = make(map[string]interface{})
-	data["timeout"] = 20
+		var data = make(map[string]interface{})
+		data["collection"] = "yikes"
+		data["schema"] = schema
+		data["interval"] = 10
+		data["source"] = "https://google.es"
+		data["method"] = "GET"
+		data["requestBody"] = make(map[string]interface{})
+		data["timeout"] = 20
 
-	var c = core.GetCore()
+		var c = core.GetCore()
 
-	var updater = c.CreateUpdater(data)
-	assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
-	assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
+		var updater = c.CreateUpdater(data)
+		assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
+		assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
 
-	c.StopUpdater(updater)
-	assert.Equal(t, len(c.Updaters), 1, "Updater not stopped")
+		c.StopUpdater(updater)
+		assert.Equal(t, len(c.Updaters), 1, "Updater not stopped")
 
-	c.RemoveUpdater(updater)
+		c.RemoveUpdater(updater)
+	}
 }
 
 func TestCoreRemoveUpdater(t *testing.T) {
-	var schema = make(map[string]interface{})
-	schema["cool"] = "test"
+	if !utils.RunningInDocker() {
+		var schema = make(map[string]interface{})
+		schema["cool"] = "test"
 
-	var data = make(map[string]interface{})
-	data["collection"] = "yikes123"
-	data["schema"] = schema
-	data["interval"] = 10
-	data["source"] = "https://google.es"
-	data["method"] = "GET"
-	data["requestBody"] = make(map[string]interface{})
-	data["timeout"] = 20
+		var data = make(map[string]interface{})
+		data["collection"] = "yikes123"
+		data["schema"] = schema
+		data["interval"] = 10
+		data["source"] = "https://google.es"
+		data["method"] = "GET"
+		data["requestBody"] = make(map[string]interface{})
+		data["timeout"] = 20
 
-	var c = core.GetCore()
+		var c = core.GetCore()
 
-	var updater = c.CreateUpdater(data)
-	assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
-	assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
+		var updater = c.CreateUpdater(data)
+		assert.NotEqual(t, updater, uuid.Nil, "Updater creation via Core failed")
+		assert.Equal(t, len(c.Updaters), 1, "There is more than one updater created")
 
-	c.RemoveUpdater(updater)
-	assert.Equal(t, len(c.Updaters), 0, "Updater not stopped")
+		c.RemoveUpdater(updater)
+		assert.Equal(t, len(c.Updaters), 0, "Updater not stopped")
+	}
 }
